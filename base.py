5197902d2477c8cafb90b0c3d960ee841452fae6
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2023-03-29 11:14:56 +0300

    Add files via upload

---
+++ base.py
@@ -0,0 +1,310 @@
+################## Modern Java Script
+part_1={
+        1: {
+            "name": "1. React in 100 Seconds",
+            "id": "BAACAgIAAx0CdCu1oAADUWQivPBZilB6zhvoJI38THjOJ0q0AAL_LAAC-K4YSaYFzgH1fV1fLwQ"
+        },
+        2: {
+            "name": "2. Anatomy",
+            "id": "BAACAgIAAx0CdCu1oAADVGQivXN0yYviQfVmPwbhRMPxNqdkAAMtAAL4rhhJw71SCuDBXlgvBA"
+        },
+        3: {
+            "name": "3.Components",
+            "id": "BAACAgIAAx0CdCu1oAADVWQiyQk6-RBszxwnenCC5wAB5m9vWgACei0AAviuGEm6ikDSa7O_Gy8E"
+        },
+        4: {
+            "name": "4. Conditional Rendering ",
+            "id": "BAACAgIAAx0CdCu1oAADVmQiySGRmCYCOvcNHVHM9GfUujnHAAJ-LQAC-K4YSbIvzZez9Y4FLwQ"
+        },
+        5: {
+            "name": "5. Loops",
+            "id": "BAACAgIAAx0CdCu1oAADV2QiyS8GJYEaQh69QGQz4F4UyYKvAAKBLQAC-K4YST03RxZuWPgYLwQ"
+        },
+        6: {
+            "name": "6.Events",
+            "id": "BAACAgIAAx0CdCu1oAADWGQiyUZ38UEl9gKvNo4TTDPmB_-FAAKELQAC-K4YSUMHEzts0A6kLwQ"
+        },
+        7: {
+            "name": "7. State",
+            "id": "BAACAgIAAx0CdCu1oAADWWQiynMAASX97C4PEujX9TAmd9RyzAACji0AAviuGEmOX7VlRwEF9y8E"
+        },
+        8: {
+            "name": "8. Lifecycle and Effects",
+            "id": "BAACAgIAAx0CdCu1oAADWmQiyoIkCRRomiSGkWhcS23GxbsZAAKQLQAC-K4YSQffNYDEVVwjLwQ"
+        }
+    }
+
+part_2={
+    1: {
+        "name": "9. Context ",
+        "id": "BAACAgIAAx0CdCu1oAADW2QiywiMt8cLyBG4AfAtApfg2WndAAKTLQAC-K4YSfuEfbY6Ejp3LwQ"
+    },
+    2: {
+        "name": "10. Error Boundries",
+        "id": "BAACAgIAAx0CdCu1oAADXGQiyxNkqTR9kpeqKiMsMZUTrDeuAAKULQAC-K4YSaX7K_e1leS8LwQ"
+    },
+    3: {
+        "name": "11. Next.js in 100 Seconds",
+        "id": "BAACAgIAAx0CdCu1oAADXWQiy0Ia-AdPE86vO9u-oqnbu1YiAAKWLQAC-K4YSQ2i2RRpQr0eLwQ"
+    },
+    4: {
+        "name": "12. Prisma in 100 Seconds",
+        "id": "BAACAgIAAx0CdCu1oAADXmQiy18lnwRaMDOm5c1sCr0QZB6eAAKXLQAC-K4YSVWcwIi5-aDFLwQ"
+    },
+    5: {
+        "name": "13. Vite in 100 Seconds",
+        "id": "BAACAgIAAx0CdCu1oAADX2Qiy5sP1hNIEAAB9n2QiqjJKiTZmAACmC0AAviuGElDCNgL48g6bC8E"
+    },
+    6: {
+        "name": "14. React Query in 100 Seconds",
+        "id": "BAACAgIAAx0CdCu1oAADYGQiy8Dn4g_5uHKTtZd4mlau24hHAAKZLQAC-K4YSUpO3rxFUtSjLwQ"
+    },
+    7: {
+        "name": "15. Progressive Web Apps in 100 Seconds",
+        "id": "BAACAgIAAx0CdCu1oAADYWQiy-q_UZTSuuvcGjOjz_CdzWP0AAKaLQAC-K4YSeX3sSWCaEH7LwQ"
+    },
+    8: {
+        "name": "16. Redux in 100 Seconds",
+        "id": "BAACAgIAAx0CdCu1oAADYmQiy_v0CCCGCI8gQMUP8vRJSuncAAKbLQAC-K4YSXw4YtIRx2ZMLwQ"
+    }
+}
+
+animal_farm={
+    1: {
+        "name": "1. Setup ",
+        "id": "BAACAgIAAx0CdCu1oAADY2QizOBJFhRev9i73szWW7eEXnKeAAKgLQAC-K4YSTW1wzl48trCLwQ"
+    },
+    2: {
+        "name": "2. Express Server",
+        "id": "BAACAgIAAx0CdCu1oAADZGQizP0Br8_OCvCgeXR0hlnFu2iWAAKjLQAC-K4YSYItSgI9HbHBLwQ"
+    },
+    3: {
+        "name": "3. Search Frontend",
+        "id": "BAACAgIAAx0CdCu1oAADZWQizSxIY5b7rzEsvpakb_tsTqRlAAKkLQAC-K4YSQrhipZWus8aLwQ"
+    }
+}
+
+
+#################### React Js
+basic_concepts={
+        1:{
+            "name":"1. Resources ",
+            "id":"BAACAgIAAx0CdCu1oAADOGQiqge3bsYqUAWGCboHuRILduw0AAIhLAAC-K4YSRpJZ7Oci09NLwQ"
+        },
+        2: {
+            "name": "2. Java Script in 100 Seconds ",
+            "id": "BAACAgIAAx0CdCu1oAADOWQiqlFvH9nLhXfrBd5bX_w7wBoUAAIjLAAC-K4YSdhe5kHFPCrzLwQ"
+        },
+        3: {
+                "name": "3.  JavaScript Crash Course ",
+                "id": "BAACAgIAAx0CdCu1oAADOmQiqsdpRzhKqVvoUW_DBwlaS3BrAAItLAAC-K4YSbOWYzt3jUZZLwQ"
+            }
+    }
+
+advanced_concepts={
+    1:{
+        "name": "1. Prototype Chain ",
+        "id": "BAACAgIAAx0CdCu1oAADO2QirR7IPdw2fKw7ZHFsb_dfbkhEAAJOLAAC-K4YSTEdd0KeH9WvLwQ"
+    },
+    2:{
+            "name": "2. Destructuring ",
+            "id": "BAACAgIAAx0CdCu1oAADPGQirV3RRXRr0p0NTUz99XNkG6T0AAJSLAAC-K4YSVE4gXrVkqY-LwQ"
+    },
+    3: {
+        "name": "3. Spread  ",
+        "id": "BAACAgIAAx0CdCu1oAADPWQirYB5CfHSaHTKwb7sAAGnQf-2FwACUywAAviuGEm4aF9gXzL46i8E"
+    },
+    4: {
+        "name": "4. Optional Chaining ",
+        "id": "BAACAgIAAx0CdCu1oAADPmQirdilrxrYp0RHGWaH7IPTCqGnAAJYLAAC-K4YSd3VSva4D8UbLwQ"
+    },
+    5: {
+            "name": "5. Nullish Coalescing ",
+            "id": "BAACAgIAAx0CdCu1oAADP2QirfLWcSZhMYEVWIn-rNe8wkxCAAJaLAAC-K4YScyj004yxTlzLwQ"
+        },
+    6: {
+                "name": "6. Higher Order Functions  ",
+                "id": "BAACAgIAAx0CdCu1oAADQGQir8OTmUnwI4RQd_jLOZqRmSj8AAJtLAAC-K4YSQrEJRWsNmxtLwQ"
+            },
+    7: {
+        "name": "7.  Closures ",
+        "id": "BAACAgIAAx0CdCu1oAADQWQir_CEDG_GBig2EjdwzHBMv_jmAAJwLAAC-K4YSctomtyND_p7LwQ"
+    },
+    8: {
+            "name": "8. Array Tricks",
+            "id": "BAACAgIAAx0CdCu1oAADQmQisAEKpD7qAm5oWT1joupWvWvwAAJxLAAC-K4YScU0nTR3e7FfLwQ"
+        },
+    9: {
+                "name": "9. History of JavaScript",
+                "id": "BAACAgIAAx0CdCu1oAADQ2QisK209i-6xep4NFzmi1lTV2r0AAJ7LAAC-K4YSaYKjxL4tOMkLwQ"
+            }
+}
+
+algorithms={
+    1: {
+        "name": "1. Comulative Sum  ",
+        "id": "BAACAgIAAx0CdCu1oAADRGQisqWvhuxBgf1hscknnLTL8mCcAAKcLAAC-K4YSffCWHeVEjhuLwQ"
+    },
+    2: {
+        "name": "2. Binary Search ",
+        "id": "BAACAgIAAx0CdCu1oAADRWQisr-jxbGTWeRxpzQZZb3eq6RqAAKfLAAC-K4YSX7G-dlj1qosLwQ"
+    },
+    3: {
+        "name": "3. Least Recently Used (LRU) Cache ",
+        "id": "BAACAgIAAx0CdCu1oAADRmQisu_y7gckHye3ugcpTAYTUDqVAAKjLAAC-K4YSarLal0N6G-ALwQ"
+    },
+    4: {
+        "name": "4. TDD with Vitest",
+        "id": "BAACAgIAAx0CdCu1oAADR2QisxWJLLx_KD2H3Zlednl8gdxwAAKlLAAC-K4YSXNMKVisLB-GLwQ"
+    }
+}
+dream_app={
+    1: {
+        "name": "1. Initial Setup ",
+        "id": "BAACAgIAAx0CdCu1oAADSGQitEZ8Abm3Z9O781QggRnCyucRAAK6LAAC-K4YSfDfqpjfWhHvLwQ"
+    },
+    2: {
+        "name": "2. RESTful APIs ",
+        "id": "BAACAgIAAx0CdCu1oAADSWQitHD43DQMElzTVvFfiaAzDyFaAAK8LAAC-K4YSeSoeXWwxgABpi8E"
+    },
+    3: {
+        "name": "3. Text-to-Image Server ",
+        "id": "BAACAgIAAx0CdCu1oAADSmQitKa1BiZKMQLwRMDFO9bxEEHgAALALAAC-K4YSaNexrnyn_kjLwQ"
+    },
+    4: {
+        "name": "4. Rest Client	",
+        "id": "BAACAgIAAx0CdCu1oAADS2QitLmzvqx4EhmsXBp_SreiqbArAALBLAAC-K4YSclSbNmYggY4LwQ"
+    },
+    5: {
+        "name": "5. Text-to-Image Frontend UI ",
+        "id": "BAACAgIAAx0CdCu1oAADTGQitOvRmuUpLF_H1PRtU77tFgT0AALCLAAC-K4YSQ3iqOm_fPzALwQ"
+    },
+    6: {
+        "name": "6. Loading Spinner ",
+        "id": "BAACAgIAAx0CdCu1oAADTWQitQiwD-n5cg8LmiQj4gOPV9TWAALDLAAC-K4YSSzilzcNm7OgLwQ"
+    },
+    7: {
+        "name": "7. Error Handling ",
+        "id": "BAACAgIAAx0CdCu1oAADTmQitUM6b2qi_WgY8tQREnQfj3RVAALELAAC-K4YSZNSAbjfWl-uLwQ"
+    }
+}
+################## Firebase Data Modelling
+# "name": "",
+# "id": ""
+
+intro={
+    1:{
+         "name": "1.Resources",
+         "id": "BAACAgIAAx0CdCu1oAADuWQj5kVzLXVEQ6I5LdMa3P9cHUU2AAKsJgAC4dcgSQr5zStlObiFLwQ"
+    },
+    2: {
+        "name": "2. What is Firestore",
+        "id": "BAACAgIAAx0CdCu1oAADumQj5naRmM7UEUDHlZGw0MYq5jBjAAKxJgAC4dcgSdFa0TetfpO_LwQ"
+    },
+    3: {
+        "name": "3.Firestore vs SQL",
+        "id": "BAACAgIAAx0CdCu1oAADu2Qj5q8-uFAuJjUfPKOhRcR3VQhQAAK3JgAC4dcgST4dvqf3-xGeLwQ"
+    },
+
+}
+
+core_concepts={
+    1:{
+        "name": "1. Data Types",
+        "id": "BAACAgIAAx0CdCu1oAADvGQj5v6ATS5SU0AmxYbM0JUrTQWZAAKqKAACmUkhSbZDvNKv4DxgLwQ"
+    },
+    2: {
+        "name": "2.Techniques",
+        "id": "BAACAgIAAx0CdCu1oAADvWQj52CMoCyAdAwKCC-eImXgkhyEAAK6JgAC4dcgSdy507_ewXgnLwQ"
+    },
+    3: {
+        "name": "3.Read",
+        "id": "BAACAgIAAx0CdCu1oAADv2Qj59JYBvbpLfv_TbyuAAHk-CovzwACuyYAAuHXIEnh-Mv84BebMS8E"
+    },
+    4: {
+        "name": "4.Query",
+        "id": "BAACAgIAAx0CdCu1oAADwGQj6CP3k_UGXLyj0WgOhveGc-GwAAK9JgAC4dcgSaSprtsluGhwLwQ"
+    },
+    5: {
+        "name": "5.Indexes",
+        "id": "BAACAgIAAx0CdCu1oAADwWQj6DJeuCuXubb_pg75qGjHS3fmAAK_JgAC4dcgSUaTHli02rySLwQ"
+    },
+    6: {
+        "name": "6.Security",
+        "id": "BAACAgIAAx0CdCu1oAADwmQj6EKWVpwS4Rst5EX9XTLIcPQCAALAJgAC4dcgSczYXuCJlkh4LwQ"
+    },
+
+}
+
+
+relational_data_modeling={
+    1:{
+        "name": "1.Cardinality",
+        "id": "BAACAgIAAx0CdCu1oAADw2Qj6ItZD1fmPXJWjokr0O3Cs3Y6AALEJgAC4dcgScdAS2G2tlh5LwQ"
+    },
+    2: {
+        "name": "2.One-to-One",
+        "id": "BAACAgIAAx0CdCu1oAADxGQj6KN0bMGkXNJV7t2mwG2kElQ2AALGJgAC4dcgSYHIrfh_pphoLwQ"
+    },
+    3: {
+        "name": "3.One-to-Many",
+        "id": "BAACAgIAAx0CdCu1oAADxWQj6NOtRt7vZeRbbCFo55ovDJZ8AALHJgAC4dcgSWvYzN6zrgeqLwQ"
+    },
+    4: {
+        "name": "4.Many-to-Many",
+        "id": "BAACAgIAAx0CdCu1oAADxmQj6QijF_qVHEPMsvlOojYmZVCGAALJJgAC4dcgSakPiCxTEGX9LwQ"
+    }
+}
+
+
+advanced_techniques={
+    1:{
+        "name": "1.Duplication",
+        "id": "BAACAgIAAx0CdCu1oAADx2Qj6nVemHKxs0uXXXphfWUfObryAALOJgAC4dcgSQ8B7k7CryRsLwQ"
+    },
+    2: {
+        "name":"2.Aggregation",
+        "id":"BAACAgIAAx0CdCu1oAADyGQj6pPxp_ujxsjTuylt6SpWxbOKAALPJgAC4dcgSdeSL_SzppMtLwQ"
+    }
+}
+
+example_models={
+    1:{
+         "name":"1.Votes",
+         "id":"BAACAgIAAx0CdCu1oAADyWQj6so9JaBzsy3m-AubA1GEqq5fAALRJgAC4dcgSbkDQaczTNVXLwQ"
+
+    },
+    2: {
+        "name":"2.Role-Based Authorization",
+        "id":"BAACAgIAAx0CdCu1oAADymQj6u-o1f9Vqoa1rfEw-HgxbpJHAALTJgAC4dcgSYwVlt7VlFrwLwQ"
+
+    },
+    3: {
+        "name":"3.Access Control List",
+        "id":"BAACAgIAAx0CdCu1oAADy2Qj6xB2jHW8nrgIDp5phZ_fx3o4AALUJgAC4dcgSa8rOwSbnqhqLwQ"
+
+    },
+    4: {
+        "name":"4.Hierarchy",
+        "id":"BAACAgIAAx0CdCu1oAADzGQj6zEZehKz1LskKiAWFPq85SykAALWJgAC4dcgSScv4De70ib4LwQ"
+
+    },
+    5: {
+        "name":"5.Follower Feed",
+        "id":"BAACAgIAAx0CdCu1oAADzWQj64IBP9mGkPn2FL4Jac3hsO3wAALeJgAC4dcgSa-C60Ky7vOzLwQ"
+    },
+
+}
+
+bonus={
+    1:{
+        "name":"1.Firestore Data Modeling - Five Cool Techniques",
+        "id":"BAACAgIAAx0CdCu1oAADzmQj69mKa7VpLz7JQcO3zs1csFGYAALlJgAC4dcgSeRwkxBtYIr_LwQ"
+
+    },
+    2:{
+        "name":"2.Collection Group Queries",
+        "id":"BAACAgIAAx0CdCu1oAADz2Qj7AEaJ8R4uawucUvGPRuSChf5AALoJgAC4dcgSSI5dAQHqvs1LwQ"
+    }
+
+}
