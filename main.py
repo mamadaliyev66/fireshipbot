3427e8b1513bfcb134d6a7113e3d746294b92845
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2023-03-29 11:14:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher , Bot,executor,types
 from aiogram.types import reply_keyboard,ReplyKeyboardMarkup
-bot_token="6166494743:AAE7Op1Ij5oM_cWVtoDW6OiXZC5_XCNakEE"
+from base import part_1,part_2,basic_concepts,advanced_concepts,algorithms,dream_app,animal_farm
+from base import advanced_techniques,example_models,bonus,intro,core_concepts,relational_data_modeling
+bot_token="5893385203:AAFTHHabQx_8Dpg8Q3tELFTcsUxXlRRGCiA"
 
 bot=Bot(token=bot_token)
 dp=Dispatcher(bot)
@@ -8,7 +10,8 @@ dp=Dispatcher(bot)
 try:
     menus=[
         ["Modern Java Script"],
-        ["React - The Full Course"]
+        ["React - The Full Course"],
+        ["Firestore Data Modelling"]
     ]
 
     reply_menu=ReplyKeyboardMarkup(keyboard=menus,resize_keyboard=True)
@@ -32,108 +35,7 @@ try:
 
     js_menus=ReplyKeyboardMarkup(keyboard=js_rp_menus,resize_keyboard=True)
 
-    basic_concepts={
-        1:{
-            "name":"1. Resources ",
-            "id":"BAACAgIAAx0CdCu1oAADOGQiqge3bsYqUAWGCboHuRILduw0AAIhLAAC-K4YSRpJZ7Oci09NLwQ"
-        },
-        2: {
-            "name": "2. Java Script in 100 Seconds ",
-            "id": "BAACAgIAAx0CdCu1oAADOWQiqlFvH9nLhXfrBd5bX_w7wBoUAAIjLAAC-K4YSdhe5kHFPCrzLwQ"
-        },
-        3: {
-                "name": "3.  JavaScript Crash Course ",
-                "id": "BAACAgIAAx0CdCu1oAADOmQiqsdpRzhKqVvoUW_DBwlaS3BrAAItLAAC-K4YSbOWYzt3jUZZLwQ"
-            }
-    }
 
-    advanced_concepts={
-        1:{
-            "name": "1. Prototype Chain ",
-            "id": "BAACAgIAAx0CdCu1oAADO2QirR7IPdw2fKw7ZHFsb_dfbkhEAAJOLAAC-K4YSTEdd0KeH9WvLwQ"
-        },
-        2:{
-                "name": "2. Destructuring ",
-                "id": "BAACAgIAAx0CdCu1oAADPGQirV3RRXRr0p0NTUz99XNkG6T0AAJSLAAC-K4YSVE4gXrVkqY-LwQ"
-        },
-        3: {
-            "name": "3. Spread  ",
-            "id": "BAACAgIAAx0CdCu1oAADPWQirYB5CfHSaHTKwb7sAAGnQf-2FwACUywAAviuGEm4aF9gXzL46i8E"
-        },
-        4: {
-            "name": "4. Optional Chaining ",
-            "id": "BAACAgIAAx0CdCu1oAADPmQirdilrxrYp0RHGWaH7IPTCqGnAAJYLAAC-K4YSd3VSva4D8UbLwQ"
-        },
-        5: {
-                "name": "5. Nullish Coalescing ",
-                "id": "BAACAgIAAx0CdCu1oAADP2QirfLWcSZhMYEVWIn-rNe8wkxCAAJaLAAC-K4YScyj004yxTlzLwQ"
-            },
-        6: {
-                    "name": "6. Higher Order Functions  ",
-                    "id": "BAACAgIAAx0CdCu1oAADQGQir8OTmUnwI4RQd_jLOZqRmSj8AAJtLAAC-K4YSQrEJRWsNmxtLwQ"
-                },
-        7: {
-            "name": "7.  Closures ",
-            "id": "BAACAgIAAx0CdCu1oAADQWQir_CEDG_GBig2EjdwzHBMv_jmAAJwLAAC-K4YSctomtyND_p7LwQ"
-        },
-        8: {
-                "name": "8. Array Tricks",
-                "id": "BAACAgIAAx0CdCu1oAADQmQisAEKpD7qAm5oWT1joupWvWvwAAJxLAAC-K4YScU0nTR3e7FfLwQ"
-            },
-        9: {
-                    "name": "9. History of JavaScript",
-                    "id": "BAACAgIAAx0CdCu1oAADQ2QisK209i-6xep4NFzmi1lTV2r0AAJ7LAAC-K4YSaYKjxL4tOMkLwQ"
-                }
-    }
-
-    algorithms={
-        1: {
-            "name": "1. Comulative Sum  ",
-            "id": "BAACAgIAAx0CdCu1oAADRGQisqWvhuxBgf1hscknnLTL8mCcAAKcLAAC-K4YSffCWHeVEjhuLwQ"
-        },
-        2: {
-            "name": "2. Binary Search ",
-            "id": "BAACAgIAAx0CdCu1oAADRWQisr-jxbGTWeRxpzQZZb3eq6RqAAKfLAAC-K4YSX7G-dlj1qosLwQ"
-        },
-        3: {
-            "name": "3. Least Recently Used (LRU) Cache ",
-            "id": "BAACAgIAAx0CdCu1oAADRmQisu_y7gckHye3ugcpTAYTUDqVAAKjLAAC-K4YSarLal0N6G-ALwQ"
-        },
-        4: {
-            "name": "4. TDD with Vitest",
-            "id": "BAACAgIAAx0CdCu1oAADR2QisxWJLLx_KD2H3Zlednl8gdxwAAKlLAAC-K4YSXNMKVisLB-GLwQ"
-        }
-    }
-    dream_app={
-        1: {
-            "name": "1. Initial Setup ",
-            "id": "BAACAgIAAx0CdCu1oAADSGQitEZ8Abm3Z9O781QggRnCyucRAAK6LAAC-K4YSfDfqpjfWhHvLwQ"
-        },
-        2: {
-            "name": "2. RESTful APIs ",
-            "id": "BAACAgIAAx0CdCu1oAADSWQitHD43DQMElzTVvFfiaAzDyFaAAK8LAAC-K4YSeSoeXWwxgABpi8E"
-        },
-        3: {
-            "name": "3. Text-to-Image Server ",
-            "id": "BAACAgIAAx0CdCu1oAADSmQitKa1BiZKMQLwRMDFO9bxEEHgAALALAAC-K4YSaNexrnyn_kjLwQ"
-        },
-        4: {
-            "name": "4. Rest Client	",
-            "id": "BAACAgIAAx0CdCu1oAADS2QitLmzvqx4EhmsXBp_SreiqbArAALBLAAC-K4YSclSbNmYggY4LwQ"
-        },
-        5: {
-            "name": "5. Text-to-Image Frontend UI ",
-            "id": "BAACAgIAAx0CdCu1oAADTGQitOvRmuUpLF_H1PRtU77tFgT0AALCLAAC-K4YSQ3iqOm_fPzALwQ"
-        },
-        6: {
-            "name": "6. Loading Spinner ",
-            "id": "BAACAgIAAx0CdCu1oAADTWQitQiwD-n5cg8LmiQj4gOPV9TWAALDLAAC-K4YSSzilzcNm7OgLwQ"
-        },
-        7: {
-            "name": "7. Error Handling ",
-            "id": "BAACAgIAAx0CdCu1oAADTmQitUM6b2qi_WgY8tQREnQfj3RVAALELAAC-K4YSZNSAbjfWl-uLwQ"
-        }
-    }
     react_rp_keyboard=[
         ["Part 1"],
         ["Part 2"],
@@ -141,92 +43,17 @@ try:
         ["🔙Back"]
     ]
 
+    firebase_dm=[
+        ['1.Intro'],
+        ["2.Core Concepts"],
+        ["3.Relational Data Modeling"],
+        ["4.Advanced Techniques"],
+        ["5.Example Models"],
+        ["6.Bonus Round"],
+        ["🔙Back"]
+    ]
 
-    part_1={
-        1: {
-            "name": "1. React in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADUWQivPBZilB6zhvoJI38THjOJ0q0AAL_LAAC-K4YSaYFzgH1fV1fLwQ"
-        }, 
-        2: {
-            "name": "2. Anatomy",
-            "id": "BAACAgIAAx0CdCu1oAADVGQivXN0yYviQfVmPwbhRMPxNqdkAAMtAAL4rhhJw71SCuDBXlgvBA"
-        },
-        3: {
-            "name": "3.Components",
-            "id": "BAACAgIAAx0CdCu1oAADVWQiyQk6-RBszxwnenCC5wAB5m9vWgACei0AAviuGEm6ikDSa7O_Gy8E"
-        },
-        4: {
-            "name": "4. Conditional Rendering ",
-            "id": "BAACAgIAAx0CdCu1oAADVmQiySGRmCYCOvcNHVHM9GfUujnHAAJ-LQAC-K4YSbIvzZez9Y4FLwQ"
-        },
-        5: {
-            "name": "5. Loops",
-            "id": "BAACAgIAAx0CdCu1oAADV2QiyS8GJYEaQh69QGQz4F4UyYKvAAKBLQAC-K4YST03RxZuWPgYLwQ"
-        },
-        6: {
-            "name": "6.Events",
-            "id": "BAACAgIAAx0CdCu1oAADWGQiyUZ38UEl9gKvNo4TTDPmB_-FAAKELQAC-K4YSUMHEzts0A6kLwQ"
-        },
-        7: {
-            "name": "7. State",
-            "id": "BAACAgIAAx0CdCu1oAADWWQiynMAASX97C4PEujX9TAmd9RyzAACji0AAviuGEmOX7VlRwEF9y8E"
-        },
-        8: {
-            "name": "8. Lifecycle and Effects",
-            "id": "BAACAgIAAx0CdCu1oAADWmQiyoIkCRRomiSGkWhcS23GxbsZAAKQLQAC-K4YSQffNYDEVVwjLwQ"
-        }   
-    }
-
-    part_2={
-        1: {
-            "name": "9. Context ",
-            "id": "BAACAgIAAx0CdCu1oAADW2QiywiMt8cLyBG4AfAtApfg2WndAAKTLQAC-K4YSfuEfbY6Ejp3LwQ"
-        }, 
-        2: {
-            "name": "10. Error Boundries",
-            "id": "BAACAgIAAx0CdCu1oAADXGQiyxNkqTR9kpeqKiMsMZUTrDeuAAKULQAC-K4YSaX7K_e1leS8LwQ"
-        },
-        3: {
-            "name": "11. Next.js in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADXWQiy0Ia-AdPE86vO9u-oqnbu1YiAAKWLQAC-K4YSQ2i2RRpQr0eLwQ"
-        },
-        4: {
-            "name": "12. Prisma in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADXmQiy18lnwRaMDOm5c1sCr0QZB6eAAKXLQAC-K4YSVWcwIi5-aDFLwQ"
-        },
-        5: {
-            "name": "13. Vite in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADX2Qiy5sP1hNIEAAB9n2QiqjJKiTZmAACmC0AAviuGElDCNgL48g6bC8E"
-        },
-        6: {
-            "name": "14. React Query in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADYGQiy8Dn4g_5uHKTtZd4mlau24hHAAKZLQAC-K4YSUpO3rxFUtSjLwQ"
-        },
-        7: {
-            "name": "15. Progressive Web Apps in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADYWQiy-q_UZTSuuvcGjOjz_CdzWP0AAKaLQAC-K4YSeX3sSWCaEH7LwQ"
-        },
-        8: {
-            "name": "16. Redux in 100 Seconds",
-            "id": "BAACAgIAAx0CdCu1oAADYmQiy_v0CCCGCI8gQMUP8vRJSuncAAKbLQAC-K4YSXw4YtIRx2ZMLwQ"
-        }   
-    }
-
-    animal_farm={
-        1: {
-            "name": "1. Setup ",
-            "id": "BAACAgIAAx0CdCu1oAADY2QizOBJFhRev9i73szWW7eEXnKeAAKgLQAC-K4YSTW1wzl48trCLwQ"
-        },
-        2: {
-            "name": "2. Express Server",
-            "id": "BAACAgIAAx0CdCu1oAADZGQizP0Br8_OCvCgeXR0hlnFu2iWAAKjLQAC-K4YSYItSgI9HbHBLwQ"
-        },
-        3: {
-            "name": "3. Search Frontend",
-            "id": "BAACAgIAAx0CdCu1oAADZWQizSxIY5b7rzEsvpakb_tsTqRlAAKkLQAC-K4YSQrhipZWus8aLwQ"
-        } 
-    }
-
+    firebase_keyboard=ReplyKeyboardMarkup(keyboard=firebase_dm,resize_keyboard=True)
 
 
     react_keyboard=ReplyKeyboardMarkup(keyboard=react_rp_keyboard,resize_keyboard=True)
@@ -298,12 +125,70 @@ try:
                 await bot.send_video(chat_id=message.from_user.id, video=animal_farm[i]['id'],
                                     caption=animal_farm[i]['name'])
 
+
+        if text=="Firestore Data Modelling":
+            await bot.send_video(chat_id=message.from_user.id, video="BAACAgIAAx0CdCu1oAADuGQj5edfnGag-qx07znP4NX662baAAKoJgAC4dcgSc6HIlcaNuQKLwQ",
+                                 caption="Firestore Data Modeling\n\n"
+                                        "Tech Stacks\n\n"
+                                        "Firebase + JavaScript\n\n"
+                                        "The Firestore Data Modeling Course provides a foundation for modeling data relationships in NoSQL, while optimizing queries for performance, cost, and complexity. Determining the the optimal data model in Cloud Firestore is not an easy task because you need to anticipate your app’s UI/UX requirements in advance. Failure to consider the tradeoffs between various data models could lead to poor app performance and/or unnecessary cloud computing expenses. The goal of this course is to teach you the concepts necessary to make good decisions related to data modeling that minimize costs and maximize read performance.\n"
+                                        "Topics Covered\n\n"
+                                        "Core Concepts in Firestore.\n\n"
+                                        "Comparisons to SQL data modeling.\n\n"
+                                        "Advanced methods for reading and querying data.\n\n"
+                                        "Relational data modeling concepts like one-to-one, one-to-many, and many-to-many.\n\n"
+                                        "How to avoid common pitfalls and anti-patterns.\n\n"
+                                        "Examples of data models for common real-world app features.\n\n",reply_markup=firebase_keyboard)
+        if text=="1.Intro":
+            for i in range(1,4):
+                await bot.send_video(chat_id=message.from_user.id, video=intro[i]['id'],
+                                    caption=intro[i]['name'])
+        if text == "2.Core Concepts":
+            for i in range(1, 7):
+                await bot.send_video(chat_id=message.from_user.id, video=core_concepts[i]['id'],
+                                     caption=core_concepts[i]['name'])
+        if text == "3.Relational Data Modeling":
+                for i in range(1, 5):
+                    await bot.send_video(chat_id=message.from_user.id, video=relational_data_modeling[i]['id'],
+                                         caption=relational_data_modeling[i]['name'])
+
+        if text == "4.Advanced Techniques":
+            for i in range(1, 3):
+                await bot.send_video(chat_id=message.from_user.id, video=advanced_techniques[i]['id'],
+                                     caption=advanced_techniques[i]['name'])
+        if text == "5.Example Models":
+            for i in range(1, 6):
+                await bot.send_video(chat_id=message.from_user.id, video=example_models[i]['id'],
+                                     caption=example_models[i]['name'])
+        if text == "6.Bonus Round":
+            for i in range(1, 3):
+                await bot.send_video(chat_id=message.from_user.id, video=bonus[i]['id'],
+                                     caption=bonus[i]['name'])
+
+
 except:
     async def error(message:types.Message):
         await message.answer(
             f"Something we went wrong ❗️\n"
         )
 
+
+
+# Define the handler function for messages from groups
+async def forward_video_from_group(message: types.Message):
+    # Check if the message has a video
+    print(message.text)
+    if message.video is not None:
+        # Forward the video to the user who sent the message to the bot
+        # await bot.send_video(chat_id=message.from_user.id, video=message.video.file_id, caption=message.caption)
+
+        await bot.send_message(chat_id=1234715065,text=f'"name":"{message.video.file_name[:len(message.video.file_name)-4]}","id":"{message.video.file_id}"')
+        print(message.caption,message.video.file_id)
+    else:
+        await bot.send_message(chat_id=1234715065, text=f'{message.text}')
+
+# Add the handler to the dispatcher
+dp.register_message_handler(forward_video_from_group, content_types=[types.ContentType.VIDEO], chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 
 
 
